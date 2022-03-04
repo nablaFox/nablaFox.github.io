a416cefe473bdb847b8b9890a16b3e2bbe1fd0f0
nablaFox <98312421+nablaFox@users.noreply.github.com>
2022-03-05 00:06:10 +0100

    Add files via upload

---
+++ fireworks/setup.js
@@ -0,0 +1,37 @@
+const container = document.querySelector('.fireworks-container')
+
+const fireworks = new Fireworks(container, {
+    target: container,
+    hue: 120,
+    startDelay: 1,
+    minDelay: 20,
+    maxDelay: 30,
+    speed: 4,
+    acceleration: 1.05,
+    friction: 0.98,
+    gravity: 1,
+    particles: 75,
+    trace: 3,
+    explosion: 5,
+    boundaries: {
+        top: 50,
+        bottom: container.clientHeight,
+        left: 50,
+        right: container.clientWidth
+    },
+    sound: {
+        enabled: true,
+        files: [
+            'https://fireworks.js.org/sounds/explosion0.mp3',
+            'https://fireworks.js.org/sounds/explosion1.mp3',
+            'https://fireworks.js.org/sounds/explosion2.mp3'
+        ],
+        volume: { min: 1, max: 9 },
+      }
+})
+
+function StartFireworks() {
+    fireworks.start();
+    FadeOutElem(['signature', 'heart', 'love-text']);
+    document.body.style.animation = "changebg 4s forwards";
+}
