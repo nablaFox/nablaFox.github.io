fbe576ca9d63ecb4a873dd6261ca93356c03a802
nablaFox <98312421+nablaFox@users.noreply.github.com>
2022-03-05 14:16:48 +0100

    Add files via upload

--- general/check-pass.js
+++ general/check-pass.js
@@ -4,16 +4,11 @@ var run = true;
 function pass(myPage) {
     document.getElementById("navbar").style.display = "none";
     document.getElementById("pass").style.display = "block";
-    setInterval(check, 10);
     page = myPage;
-    if (run == true)
-        setInterval(check, 1000);
 }
 
 function check() {
     if (document.querySelector('input').value == '1234') {
-        document.querySelector('input').value == '';
         window.open(page,'_self');
-        run = false;
     }
 }
