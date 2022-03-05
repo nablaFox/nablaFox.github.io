27d6cd00508d2b3f450d88144b0f4299c8cf849b
nablaFox <98312421+nablaFox@users.noreply.github.com>
2022-03-05 13:58:39 +0100

    Update check-pass.js

--- general/check-pass.js
+++ general/check-pass.js
@@ -1,14 +1,18 @@
 var page;
+var run = true;
 
 function pass(myPage) {
     document.getElementById("navbar").style.display = "none";
     document.getElementById("pass").style.display = "block";
     setInterval(check, 10);
     page = myPage;
+    if (run == true)
+        setInterval(check, 1000);
 }
 
 function check() {
     if (document.querySelector('input').value == '1234') {
         window.open(page,'_self');
+        run = false;
     }
 }
